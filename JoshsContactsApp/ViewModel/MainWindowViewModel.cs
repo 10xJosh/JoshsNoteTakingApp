94802381f16e92abaf124624a9b175e242e92225
10xJosh <55113224+10xJosh@users.noreply.github.com>
2023-10-27 06:45:42 -0700

    started ground work for INotifyPropertyChanged

--- JoshsContactsApp/ViewModel/MainWindowViewModel.cs
+++ JoshsContactsApp/ViewModel/MainWindowViewModel.cs
@@ -1,19 +1,47 @@
-﻿using SQLite;
+﻿using JoshsContactsApp.Model;
+using SQLite;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace JoshsContactsApp.ViewModel
 {
-	internal class MainWindowViewModel
+	class MainWindowViewModel : INotifyPropertyChanged
 	{
-        public MainWindowViewModel()
-        {
-			
+		private string Title;
+
+		public string Query
+		{
+			get { return Title; }
+			set 
+			{
+				OnPropertyChanged("Query"); 
+			}
 		}
 
+
+		public event PropertyChangedEventHandler? PropertyChanged;
+
+		private void OnPropertyChanged(string propertyName)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+
+		public static List<Note> GetNotes()
+		{
+			List<Note> notes = new List<Note>();
+
+			return notes;
+		}
+
+		private void test()
+		{
+			MessageBox.Show("Yeeahw");
+		}
     }
 }
